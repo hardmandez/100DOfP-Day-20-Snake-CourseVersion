ec77a68f87b22d74b88ffc6618c367fa448c0bf1
joseh <hardmandez@hotmail.com>
2025-11-05 06:49:10 +0000

    051125 0649

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
+
 
 #Set up game screen.
 screen=Screen()
@@ -9,22 +11,25 @@ screen.title("My Snake Game")
 #Switch of the screen update so can update screen manually
 screen.tracer(0)
 
+
+#Setup snake.
+snake  = Snake()
 #Define the starting position of the three objects to be created.
-starting_positions=[(0,0),(-20,0),(-40,0)]
+# starting_positions=[(0,0),(-20,0),(-40,0)]
 
 #Define empty list.
-segments = []
+# segments = []
 
 #Cycle throught the start position tuples and create a turtle object.
-for position in starting_positions:
-    #Create a new segment.
-    new_segment=Turtle("square")
-    new_segment.penup()
-    new_segment.color("white")
-    #Send the segment to the position in the list
-    new_segment.goto(position)
-    #Add the segment to list.
-    segments.append(new_segment)
+# for position in starting_positions:
+#     #Create a new segment.
+#     new_segment=Turtle("square")
+#     new_segment.penup()
+#     new_segment.color("white")
+#     #Send the segment to the position in the list
+#     new_segment.goto(position)
+#     #Add the segment to list.
+#     segments.append(new_segment)
 
 
 game_is_on = True
@@ -34,20 +39,7 @@ while game_is_on:
     #Sleep to affect the screen update speed\fluidity.
     time.sleep(0.1)
 
-    #For each number in range(range is the size of the segments list - 1 to 0, this is the revers.  Step is -1 to go backwards in range
-    for seg_num in range(len(segments)-1,0,-1):
-        #Get the previous segment x and set as new x
-        new_x=segments[seg_num-1].xcor()
-        #Get the previous segment y and set as new y.
-        new_y=segments[seg_num-1].ycor()
-        #Send current segment to previous segments position.
-        segments[seg_num].goto(new_x,new_y)
-
-    #move segments.
-    segments[0].forward(20)
-    segments[0].left(10)
-
-
+    snake.move_snake()
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,24 +1,43 @@
-from main import starting_positions
 from turtle import Turtle, Screen
+
+#Constants defined outside of class and in capitals.
+START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
 
 class Snake:
 
     def __init__(self):
-        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-        segments = []
-        new_segment = Turtle("square")
-        new_segment.penup()
-        new_segment.color("white")
+        #Define empty list.
+        self.segments = []
 
-        # Define empty list.
+        #Call method to create snake.
+        self.create_snake()
+        self.move_snake()
 
-    def move_snake(self):
 
+    def create_snake(self):
         # Cycle throught the start position tuples and create a turtle object.
-        for position in starting_positions:
+        for position in START_POSITIONS:
             # Create a new segment.
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
 
             # Send the segment to the position in the list
-            self.new_segment.goto(position)
+            new_segment.goto(position)
             # Add the segment to list.
             self.segments.append(new_segment)
+
+    def move_snake(self):
+        # For each number in range(range is the size of the segments list - 1 to 0, this is the revers.  Step is -1 to go backwards in range
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # Get the previous segment x and set as new x
+            new_x = self.segments[seg_num - 1].xcor()
+            # Get the previous segment y and set as new y.
+            new_y = self.segments[seg_num - 1].ycor()
+            # Send current segment to previous segments position.
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+        self.segments[0].left(10)
