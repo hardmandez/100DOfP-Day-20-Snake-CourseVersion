a7ee9f5cee3d7b105760bc1f27105b666f4c3a2e
joseh <hardmandez@hotmail.com>
2025-11-05 04:25:08 +0000

    051125

--- snake.py
+++ snake.py
@@ -1,19 +1,24 @@
+from main import starting_positions
+from turtle import Turtle, Screen
+
 class Snake:
-    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
     def __init__(self):
-
+        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+        segments = []
+        new_segment = Turtle("square")
+        new_segment.penup()
+        new_segment.color("white")
 
         # Define empty list.
-        segments = []
+
+    def move_snake(self):
 
         # Cycle throught the start position tuples and create a turtle object.
         for position in starting_positions:
             # Create a new segment.
-            new_segment = Turtle("square")
-            new_segment.penup()
-            new_segment.color("white")
+
             # Send the segment to the position in the list
-            new_segment.goto(position)
+            self.new_segment.goto(position)
             # Add the segment to list.
-            segments.append(new_segment)
+            self.segments.append(new_segment)
